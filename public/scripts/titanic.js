e2a3ee8b6510d03948f46b8e18adcf8ff649f056
Darkweld <Darkweld@users.noreply.github.com>
2018-06-02 07:05:59 -0400

    experimenting with refs

--- public/scripts/titanic.js
+++ public/scripts/titanic.js
@@ -9,18 +9,32 @@ import loremIpsum from "../assets/texts/loremipsum.js";
 class Main extends React.Component {
   constructor() {
     super();
-    this.state = {submerged: 500};
+
+    let items = loremIpsum.map((v, i) => <p key = {i} className = {style.descriptionText}>{v}</p>);
+    this.state = {submerged: 500, items: items, bottom: document.body.scrollHeight};
+    this.textContainerRef = React.createRef();
   }
   componentDidMount () {
     // get items heights and wrapper/body height, then determine whether a div or object is submerged.
-
+    let array = [...this.textContainerRef.current.childNodes];
+    let items = array.map((v, i) => {
+      console.log(v.getBoundingClientRect().top, this.state.bottom);
+      if (v.getBoundingClientRect().top > this.state.bottom - this.state.submerged) {
+        if (v.tagName === "P") return <p key = {i} className = {style.textSubmerged}>{v.textContent}</p>
+    }
+    });
+    this.setState({items: items});
   }
 
 
   render() {
+
+
     return(
       <div className = {style.wrapper}>
-        {loremIpsum.map((v, i) => <p key = {i}>{v}</p>)}
+        <div className = {style.textContainer} ref = {this.textContainerRef}>
+          {this.state.items}
+        </div>
         <div className = {style.water} style = {{height: this.state.submerged}}></div>
       </div>
 
