84fb0796c54f7fc8866930ba2d1a70509efc5c78
Darkweld <Darkweld@users.noreply.github.com>
2018-06-05 07:15:32 -0400

    trying to get correct height of p elements

--- public/scripts/titanic.js
+++ public/scripts/titanic.js
@@ -11,25 +11,32 @@ class Main extends React.Component {
     super();
 
     let items = loremIpsum.map((v, i) => <p key = {i} className = {style.descriptionText}>{v}</p>);
-    this.state = {submerged: 500, items: items, bottom: document.body.scrollHeight};
+    this.state = {submerged: 0, items: items, bottom: null, heights: null};
     this.textContainerRef = React.createRef();
+    this.updateWater = this.updateWater.bind(this);
   }
   componentDidMount () {
     // get items heights and wrapper/body height, then determine whether a div or object is submerged.
-    let array = [...this.textContainerRef.current.childNodes];
-    let items = array.map((v, i) => {
-      console.log(v.getBoundingClientRect().top, this.state.bottom);
-      if (v.getBoundingClientRect().top > this.state.bottom - this.state.submerged) {
-        if (v.tagName === "P") return <p key = {i} className = {style.textSubmerged}>{v.textContent}</p>
-    }
-    });
-    this.setState({items: items});
+    let arr = [...this.textContainerRef.current.childNodes].map((v) => [v.tagName, v.getBoundingClientRect().top]);
+    this.setState({bottom: this.textContainerRef.current.scrollHeight, heights: arr});
+    this.water = setInterval(() => this.updateWater(), 1000);
   }
 
+  componentWillUnmount() {
+    clearInterval(this.water);
+  }
+
+  updateWater() {
+    let sub = this.state.bottom - this.state.submerged;
+    let items = this.state.heights.map((v, i) => {
+      let submerged = (v[1] > sub) ? "submerged " : "";
+       if (v[0] === "P") return <p key = {i} className = {submerged + style.descriptionText}>{loremIpsum[i]}</p>
+      });
+    let max = (this.state.submerged + 25 < this.state.bottom) ? this.state.submerged + 25 : this.state.bottom;
+    this.setState({submerged: max, items: items});
+  }
 
   render() {
-
-
     return(
       <div className = {style.wrapper}>
         <div className = {style.textContainer} ref = {this.textContainerRef}>
