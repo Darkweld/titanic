83ecfbc62d4e26b7276d61d512175a93fcc04c8e
Darkweld <Darkweld@users.noreply.github.com>
2018-06-12 07:11:52 -0400

    testing height

--- public/scripts/titanic.js
+++ public/scripts/titanic.js
@@ -16,14 +16,13 @@ function TextNode ({ bool, text }) {
 
 class Content extends React.Component {
   render() {
-    let arr = (this.props.heights) ? this.props.heights.map((v, i) => {
-       if (v[0] === "P") return <TextNode key = {i} bool = {v[2]} text = {loremIpsum[i]} />
-    }) : loremIpsum.map((v, i) =>  <p key = {i} className = {style.descriptionText}>{v}</p>);
+    let arr = (this.props.submerged) ? loremIpsum.map((v, i) =>  <p key = {i} className = {style.submergedText}>{v}</p>)
+    : loremIpsum.map((v, i) =>  <p key = {i} className = {style.descriptionText}>{v}</p>);
+
   return (
-    <div ref = {this.props.contentRef} className = {style.textContainer}>
+    <div ref = {this.props.textRef} style = {{"height": this.props.height || "auto"}} className = {(this.props.submerged) ? style.textContainerSubmerged : style.textContainer}>
       {arr}
     </div>
-
   );
   }
 }
@@ -32,22 +31,14 @@ class Main extends React.Component {
   constructor() {
     super();
 
-    this.state = {submerged: 0, heights: null, scroll: 0, start: false};
-    //Refs
+    this.state = {submerged: 0, height: null, scroll: 0, start: false};
     this.textContainerRef = React.createRef();
     //Binds
     this.updateWater = this.updateWater.bind(this);
     this.scrollCalc = this.scrollCalc.bind(this);
   }
   componentDidMount () {
-    //Ok, when calculating ref children sizes while using componentDidMount, componentDidMount actually mounts before injecting CSS.
-    //So what ends up happening is that the offset tops are not correct to what I want them to do. You have to setTimeout.
-    //This shouldn't be a problem, as I am not using this in the final implementation like this.
     window.addEventListener('scroll', this.scrollCalc, false);
-
-
-    //let arr = [...this.textContainerRef.current.childNodes].map((v) => [v.tagName, v.offsetTop, false]);
-  //  this.setState({heights: arr}, () => this.water = setInterval(() => this.updateWater(), 100));
   }
 
   componentWillUnmount() {
@@ -56,19 +47,15 @@ class Main extends React.Component {
   }
 
   scrollCalc() {
-    console.log(document.documentElement.scrollTop);
-    if (document.documentElement.scrollHeight - document.documentElement.scrollTop  === document.documentElement.clientHeight) {
+    if (Math.ceil(document.documentElement.scrollHeight - document.documentElement.scrollTop) >  document.documentElement.clientHeight) {
       window.removeEventListener('scroll', this.scrollCalc, false);
-      let arr = [...this.textContainerRef.current.childNodes].map((v) => [v.tagName, v.offsetTop, false]);
-      this.setState({heights: arr, start: true}, () => this.water = setInterval(() => this.updateWater(), 100));
+      this.setState({start: true, height: this.textContainerRef.current.scrollHeight}, () => this.water = setInterval(() => this.updateWater(), 100));
     }
   }
 
   updateWater() {
-    let ref = this.textContainerRef.current;
-    let arr = this.state.heights.map((v) => [v[0], v[1], (v[1] > ref.scrollHeight - this.state.submerged) ? true : false]);
-    if (this.state.submerged > ref.scrollHeight) clearInterval(this.water);
-    this.setState({submerged: this.state.submerged + 1, heights: arr});
+    if (this.state.submerged > this.state.height) clearInterval(this.water);
+    this.setState({submerged: this.state.submerged + 1});
   }
 
   render() {
@@ -76,7 +63,8 @@ class Main extends React.Component {
 
     return(
       <div className = {style.wrapper}>
-        <Content contentRef = {this.textContainerRef} heights = {this.state.heights} />
+        <Content submerged = {true}/>
+        <Content height = {this.state.height - this.state.submerged} textRef = {this.textContainerRef}/>
         {this.state.start && <div className = {style.water} style = {{height: this.state.submerged}}></div>}
       </div>
 
